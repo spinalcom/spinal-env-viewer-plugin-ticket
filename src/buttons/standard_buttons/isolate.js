252ba9d2f3416f1e37f4acd22141e9a7a29a97a2
Laurent <l.ha-tam-dan@spinalcom.com>
2024-07-04 16:34:18 +0200

    fix select/isolate/fit button

--- src/buttons/standard_buttons/isolate.js
+++ src/buttons/standard_buttons/isolate.js
@@ -1,14 +1,34 @@
+/*
+ * Copyright 2024 SpinalCom - www.spinalcom.com
+ * 
+ * This file is part of SpinalCore.
+ * 
+ * Please read all of the following terms and conditions
+ * of the Software license Agreement ("Agreement")
+ * carefully.
+ * 
+ * This Agreement is a legally binding contract between
+ * the Licensee (as defined below) and SpinalCom that
+ * sets forth the terms and conditions that govern your
+ * use of the Program. By installing and/or using the
+ * Program, you agree to abide by all the terms and
+ * conditions stated or referenced herein.
+ * 
+ * If you do not agree to abide by these terms and
+ * conditions, do not demonstrate your acceptance and do
+ * not install or use the Program.
+ * You should have received a copy of the license along
+ * with this file. If not, see
+ * <http://resources.spinalcom.com/licenses.pdf>.
+ */
+
 import {
   SpinalContextApp
 } from "spinal-env-viewer-context-menu-service";
 
-
 import {
   SERVICE_TYPE,
-  SPINAL_TICKET_SERVICE_TICKET_TYPE,
-  SPINAL_TICKET_SERVICE_TICKET_RELATION_NAME,
 } from "spinal-service-ticket/dist/Constants";
-
 
 import ColorElementExtension from "../../extensions/colorElementExtension";
 
@@ -28,8 +48,6 @@ export class IsolateElementOnMaquette extends SpinalContextApp {
 
     if (
       contextType === SERVICE_TYPE
-      // &&
-      // nodeType === SPINAL_TICKET_SERVICE_TICKET_TYPE
     )
       return Promise.resolve(true);
     return Promise.resolve(-1);
@@ -47,10 +65,6 @@ export class IsolateElementOnMaquette extends SpinalContextApp {
       return;
     }
 
-    parents.forEach((el) => {
-      window.spinal.ForgeViewer.viewer.impl.visibilityManager.isolate(el
-        .ids, el.model)
-    })
-
+    spinal.ForgeViewer.viewer.impl.visibilityManager.aggregateIsolate(parents);
   }
 }
--- src/buttons/standard_buttons/selectElement.js
+++ src/buttons/standard_buttons/selectElement.js
@@ -1,3 +1,27 @@
+/*
+ * Copyright 2024 SpinalCom - www.spinalcom.com
+ * 
+ * This file is part of SpinalCore.
+ * 
+ * Please read all of the following terms and conditions
+ * of the Software license Agreement ("Agreement")
+ * carefully.
+ * 
+ * This Agreement is a legally binding contract between
+ * the Licensee (as defined below) and SpinalCom that
+ * sets forth the terms and conditions that govern your
+ * use of the Program. By installing and/or using the
+ * Program, you agree to abide by all the terms and
+ * conditions stated or referenced herein.
+ * 
+ * If you do not agree to abide by these terms and
+ * conditions, do not demonstrate your acceptance and do
+ * not install or use the Program.
+ * You should have received a copy of the license along
+ * with this file. If not, see
+ * <http://resources.spinalcom.com/licenses.pdf>.
+ */
+
 import {
   SpinalContextApp
 } from "spinal-env-viewer-context-menu-service";
@@ -5,26 +29,7 @@ import {
 
 import {
   SERVICE_TYPE,
-  SPINAL_TICKET_SERVICE_TICKET_TYPE,
-  SPINAL_TICKET_SERVICE_TICKET_RELATION_NAME,
 } from "spinal-service-ticket/dist/Constants";
-
-import {
-  SpinalGraphService
-} from "spinal-env-viewer-graph-service";
-
-
-import {
-  SpinalContextSelectBIMObject,
-} from "spinal-env-viewer-plugin-standard_button/js/selectBIMObjectButton";
-
-import {
-  SpinalContextFitToViewer
-} from "spinal-env-viewer-plugin-standard_button/js/fitToViewerButton";
-
-import {
-  isShownParam
-} from "spinal-env-viewer-plugin-standard_button/js/utilities";
 
 import ColorElementExtension from "../../extensions/colorElementExtension";
 
@@ -44,8 +49,6 @@ export class SelectElementOnMaquette extends SpinalContextApp {
 
     if (
       contextType === SERVICE_TYPE
-      // &&
-      // nodeType === SPINAL_TICKET_SERVICE_TICKET_TYPE
     )
       return Promise.resolve(true);
     return Promise.resolve(-1);
@@ -63,22 +66,6 @@ export class SelectElementOnMaquette extends SpinalContextApp {
       window.alert("No parent on bimMaquette");
       return;
     }
-
-    parents.forEach((el) => {
-      el.model.selector.setSelection(el.ids, el.model, "selectOnly");
-    })
-
-
-
-    // const selectButton = new SpinalContextSelectBIMObject();
-    // // const zoomButton = new SpinalContextFitToViewer();
-
-    // parents.forEach((element) => {
-    //   const params = {
-    //     selectedNode: element,
-    //   };
-    //   selectButton.action(params);
-    //   // zoomButton.action(params);
-    // });
+    spinal.ForgeViewer.viewer.impl.selector.setAggregateSelection(parents);
   }
 }
